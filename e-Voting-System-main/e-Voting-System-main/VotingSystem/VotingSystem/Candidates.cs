ec3497d2c2a8e5ef622d13863b9082c2f4bb37f9
gerushi <136679833+gerushi@users.noreply.github.com>
2024-10-31 23:04:12 +0800

    Add files via upload

---
+++ e-Voting-System-main/e-Voting-System-main/VotingSystem/VotingSystem/Candidates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace VotingSystem
+{
+    public partial class Candidates : KryptonForm
+    {
+        public Candidates()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
